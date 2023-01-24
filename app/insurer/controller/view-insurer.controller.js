3d3e3e67438624468d9f49942b30b3dbdfd1e795
usmanaryk1 <usmanaryk1@gmail.com>
2023-01-25 00:00:00 +0500

    Changes for insurance provider.

--- app/insurer/controller/view-insurer.controller.js
+++ app/insurer/controller/view-insurer.controller.js
@@ -1,11 +1,10 @@
 (function() {
     function ViewInsurersCtrl(InsurerDAO, $rootScope, $stateParams, $state, $modal) {
         var ctrl = this;
-        $rootScope.selectEmployeeModel={};
+        $rootScope.selectEmployeeModel = {};
         ctrl.retrieveInsurers = retrieveInsurersData;
         ctrl.edit = edit;
-        ctrl.deleteInsurer = deleteInsurer;
-        
+
         function retrieveInsurersData() {
             InsurerDAO.retrieveAll().then(function(res) {
                 showLoadingBar({
@@ -23,28 +22,53 @@
                     delay: .5,
                     pct: 100,
                     finish: function() {
-                        
+
                     }
                 }); // showLoadingBar
                 ctrl.insurerList = ontimetest.insuranceProviders;
             });
         }
-        
-        function edit(insurer){
+
+        function edit(insurer) {
             $state.go('app.insurer', {id: insurer.id});
         }
-        
-        function deleteInsurer(insurer){
-            var length = ctrl.insurerList.length;
-            
-            for(var i=0;i<length;i++){
-                if(ctrl.insurerList[i].id === insurer.id){
-                    ctrl.insurerList.splice(i, 1);
-                    break;
-                }
-            }
-        }
-        
+
+        ctrl.openModal = function(insurer, modal_id, modal_size, modal_backdrop)
+        {
+            $rootScope.deleteInsurerModel = $modal.open({
+                templateUrl: modal_id,
+                size: modal_size,
+                backdrop: typeof modal_backdrop == 'undefined' ? true : modal_backdrop
+            });
+            $rootScope.deleteInsurerModel.insurer = insurer;
+
+            $rootScope.deleteInsurerModel.delete = function(insurer) {
+                InsurerDAO.delete({id: insurer.id}).then(function(res) {
+                    var length = ctrl.insurerList.length;
+
+                    for (var i = 0; i < length; i++) {
+                        if (ctrl.insurerList[i].id === insurer.id) {
+                            ctrl.insurerList.splice(i, 1);
+                            break;
+                        }
+                    }
+                    $rootScope.deleteInsurerModel.close();
+                }).catch(function(data, status) {
+
+                    var length = ctrl.insurerList.length;
+
+                    for (var i = 0; i < length; i++) {
+                        if (ctrl.insurerList[i].id === insurer.id) {
+                            ctrl.insurerList.splice(i, 1);
+                            break;
+                        }
+                    }
+                    $rootScope.deleteInsurerModel.close();
+                });
+            };
+
+        };
+
         ctrl.retrieveInsurers();
 //        ctrl.openEditModal = function(employee, modal_id, modal_size, modal_backdrop)
 //        {
@@ -56,7 +80,7 @@
 //            $rootScope.selectEmployeeModel.employee=employee;
 //
 //        };
-        
+
     }
     ;
     angular.module('xenon.controllers').controller('ViewInsurersCtrl', ["InsurerDAO", "$rootScope", "$stateParams", "$state", "$modal", ViewInsurersCtrl]);
--- app/insurer/service/insurer.service.js
+++ app/insurer/service/insurer.service.js
@@ -9,9 +9,13 @@
                     action: 'view'
                 }
             },
-            //this method will be used for patient save or update based on the action passed
+            //this method will be used for insuranceProvider save or update based on the action passed
             update: {
                 method: 'POST'
+            },
+            //this method will be used for insuranceProvider delete
+            delete: {
+                method: 'GET'
             },
         });
         return {
@@ -23,6 +27,9 @@
             },
             update: function (data) {
                 return api.update({action:data.action}, data.data).$promise;
+            },
+            delete: function (data) {
+                return api.delete({action:'delete', subAction:data.id}).$promise;
             },
         };
     };
