d275a6314de7f443c3afaa5d9dc6113e5aee600b
usmanaryk1 <usmanaryk1@gmail.com>
2023-01-26 00:00:00 +0500

    dashboard tasklist changed as per requirement. remove checked task button added.

--- app/dashboard/controller/dashboard.controller.js
+++ app/dashboard/controller/dashboard.controller.js
@@ -1,10 +1,10 @@
 (function () {
     function DashboardCtrl(Page, $rootScope, $formService, $timeout, UserDAO, DashboardDAO, $filter) {
         var ctrl = this;
-        Page.setTitle("Dashboard");        
-        ctrl.weatherObj = {location: ontime_data.weatherCity,currentTime:new Date().getTime()/1000};
+        Page.setTitle("Dashboard");
+        ctrl.weatherObj = {location: ontime_data.weatherCity, currentTime: new Date().getTime() / 1000};
 //        $rootScope.isAdminPortal = false;
-        var tasks = JSON.parse(localStorage.getItem("tasks"));
+        var tasks = JSON.parse(localStorage.getItem("tasksArray"));
         ctrl.weatherIcons = {'clear-day': 'meteocons-sun', 'clear-night': 'meteocons-moon', 'rain': 'meteocons-rain', 'snow': 'meteocons-snow', 'sleet': 'meteocons-rain', 'wind': 'meteocons-wind', 'fog': 'meteocons-fog', 'cloudy': 'meteocons-cloud', 'partly-cloudy-day': 'meteocons-cloud-sun', 'partly-cloudy-night': 'meteocons-cloud-moon'}
         if (tasks != null && tasks[$rootScope.currentUser.userName] != null) {
             //this flag is used to hide the flickering from normal checkbox to theme checkbox
@@ -21,14 +21,31 @@
             $("#taskList").perfectScrollbar().addClass('overflow-hidden');
         });
         ctrl.taskMarked = function (index) {
-            ctrl.taskList.splice(index, 1);
             setTasksInLocalStorage();
+        };
+        ctrl.removeAllMarkedTasks = function () {
+            var taskListToSave = [];
+            angular.forEach(ctrl.taskList, function (task, index) {
+                if (!task.taskMarked) {
+                    taskListToSave.push(task);
+                }
+            });
+            ctrl.inProgress = true;
+            ctrl.taskList = [];
+            $timeout(function () {
+                ctrl.taskList = taskListToSave;
+                $timeout(function () {
+                    cbr_replace();
+                    ctrl.inProgress = false;
+                    setTasksInLocalStorage();
+                });
+            });            
         };
         ctrl.addTask = function () {
             if (ctrl.taskName != null && ctrl.taskName != '') {
                 //this flag is used to hide the flickering from normal checkbox to theme checkbox
                 ctrl.inProgress = true;
-                ctrl.taskList.push(ctrl.taskName);
+                ctrl.taskList.push({taskValue: ctrl.taskName});
                 $timeout(function () {
                     cbr_replace();
                     ctrl.inProgress = false;
@@ -45,7 +62,7 @@
                 tasks = {};
             }
             tasks[$rootScope.currentUser.userName] = ctrl.taskList;
-            localStorage.setItem("tasks", JSON.stringify(tasks));
+            localStorage.setItem("tasksArray", JSON.stringify(tasks));
         };
         ctrl.retrieveCounts = function () {
             DashboardDAO.getCompianceTrackerCount({currentDate: $filter('date')(new Date(), $rootScope.dateFormat)}).then(function (res) {
