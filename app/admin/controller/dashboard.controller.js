aa3070fa3068248510898099d12cac1de77dc72a
usmanaryk1 <usmanaryk1@gmail.com>
2023-01-31 00:00:00 +0500

    new icons are added for dashboard and adjusted

--- app/admin/controller/dashboard.controller.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    function AdminDashboardCtrl(Page, $rootScope) {
-        var ctrl = this;
-        Page.setTitle("Dashboard");
-    }
-    ;
-    angular.module('xenon.controllers').controller('AdminDashboardCtrl', ["Page", "$rootScope", AdminDashboardCtrl]);
-})();
