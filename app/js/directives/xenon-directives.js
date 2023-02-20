4bf2a3d1ded2756e8712c408f40fde2369302ad7
usmanaryk1 <usmanaryk1@gmail.com>
2023-02-21 00:00:00 +0500

    numbers only added.

--- app/js/directives/xenon-directives.js
+++ app/js/directives/xenon-directives.js
@@ -918,10 +918,10 @@ angular.module('xenon.directives', []).
                         }
 
                     });
-                    
-                    scope.$watch("ngModel",function(value){
-                        if(value!=null){
-                             $this.datepicker("setDate",new Date(value));
+
+                    scope.$watch("ngModel", function(value) {
+                        if (value != null) {
+                            $this.datepicker("setDate", new Date(value));
                         }
                     })
 
@@ -1132,4 +1132,65 @@ angular.module('xenon.directives', []).
                     });
                 }
             };
-        });
+        })
+        .directive('numbersOnly', ['$parse', function($parse) {
+                return {
+                    require: ['^form', 'ngModel'],
+                    link: function(scope, element, attrs, ctrls) {
+                        scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+                            var mdlctrl = ctrls[1];
+                            var model = $parse(attrs.ngModel);
+                            var arr = String(newValue).split("");
+                            if (arr.length === 0) {
+                                return;
+                            }
+                            if (arr.length === 1 && (((arr[0] === '-') &&
+                                    (attrs.negativeallowed !== undefined &&
+                                            attrs.negativeallowed.toString() === "true")) || ((arr[0] === '.') &&
+                                    (attrs.decimalallowed !== undefined && attrs.decimalallowed.toString()
+                                            === "true"))))
+                            {
+                                return;
+                            }
+                            if ((arr.length === 2 && newValue === '-.') &&
+                                    ((attrs.decimalallowed !== undefined &&
+                                            attrs.decimalallowed.toString() === "true"))) {
+                                return;
+                            }
+                            if (attrs.negativeallowed === undefined ||
+                                    attrs.negativeallowed.toString() === "false") {
+                                if ((newValue != null)) {
+                                    transformedNewValue =
+                                            newValue.toString().replace('-', '');
+                                    if (newValue !== transformedNewValue) {
+                                        model.assign(scope,
+                                                transformedNewValue);
+                                    }
+                                }
+                            }
+                            if (attrs.decimalallowed === undefined ||
+                                    attrs.decimalallowed.toString() === "false") {
+                                if ((newValue != undefined)) {
+                                    transformedNewValue =
+                                            newValue.toString().replace('.', '');
+                                    if (newValue !== transformedNewValue) {
+                                        model.assign(scope,
+                                                transformedNewValue);
+                                    }
+                                }
+                            }
+                            if (isNaN(newValue)) {
+                                if ((newValue === undefined)) {
+                                    model.assign(scope, newValue);
+                                }
+                                else {
+                                    model.assign(scope, oldValue);
+                                    mdlctrl.$setPristine();
+                                }
+
+                            }
+                        });
+                    }
+                };
+            }]);
+
