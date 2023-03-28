96ba59bd098b7df561f4677c8f8efed2d0d48294
usmanaryk1 <usmanaryk1@gmail.com>
2023-03-29 00:00:00 +0500

    Add employee next button added.

---
+++ app/payroll/controller/payroll-history.controller.js
@@ -0,0 +1,8 @@
+(function() {
+    function PayrollHistoryCtrl($rootScope, $scope, $http, $modal, $timeout) {
+        var ctrl = this;
+
+    }
+    ;
+    angular.module('xenon.controllers').controller('PayrollHistoryCtrl', ["$rootScope", "$scope", "$http", "$modal", "$timeout", PayrollHistoryCtrl]);
+})();
