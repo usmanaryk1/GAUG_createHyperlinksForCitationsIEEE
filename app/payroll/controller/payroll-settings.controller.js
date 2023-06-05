22c641643ebd012d206c3b5bca879b31a62528da
usmanaryk1 <usmanaryk1@gmail.com>
2023-06-06 00:00:00 +0500

    payroll settings completed

--- app/payroll/controller/payroll-settings.controller.js
+++ app/payroll/controller/payroll-settings.controller.js
@@ -1,5 +1,5 @@
 (function() {
-    function PayrollSettingsCtrl($rootScope, $scope, $http, $modal, $timeout, PayrollDAO) {
+    function PayrollSettingsCtrl($rootScope, $scope, $http, $modal, $timeout, PayrollDAO, $filter) {
         var ctrl = this;
         ctrl.payrollObj = {companyCode: ontimetest.company_code};
         ctrl.holidays = [{name: 'Christmas'}, {name: 'Columbus Day'}, {name: 'Independence Day'}, {name: 'Labor'}, {name: 'Memorial Day'}, {name: 'MLK Birthday'}, {name: 'New Year\'s Day'}, {name: 'Thanks giving Day'}, {name: 'Veterans Day'}, {name: 'Washington\'s Birthday'}];
@@ -13,6 +13,7 @@
                 }
                 $timeout(function() {
                     $('#multi-select').multiSelect('refresh');
+                    ctrl.setHolidayManually();
                 });
             }).catch(function() {
                 toastr.error("Payroll settings cannot be retrieved.");
@@ -43,11 +44,11 @@
         ctrl.saveSettings = saveSettings;
         //function to save the payroll settings
         function saveSettings() {
-            if ($('#payroll_settings_form')[0].checkValidity()) {
+            if ($('#payroll_settings_form')[0].checkValidity() && ctrl.payrollObj.holidays != null && ctrl.payrollObj.holidays.length > 0) {
                 $rootScope.maskLoading();
                 ctrl.payrollObj.holidays = angular.copy(ctrl.payrollObj.holidays);
-                console.log(JSON.stringify(ctrl.payrollObj));
                 PayrollDAO.updateSettings(ctrl.payrollObj).then(function(res) {
+                    ctrl.payrollObj = res;
                     console.log(res);
                     toastr.success("Payroll settings saved.");
                 }).catch(function() {
@@ -58,24 +59,46 @@
             }
         }
         ;
+        ctrl.setHolidayManually = function() {
+            if (ctrl.payrollObj.holidays != null && ctrl.payrollObj.holidays.length > 0) {
+                angular.forEach(ctrl.payrollObj.holidays, function(holiday) {
+                    if (holiday.isRepeatAnnually == false) {
+                        $(".ms-selection").find("span:contains('" + holiday.name + "')").css("color", "red");
+                    }
+                });
+            }
+        };
 // Open Simple Modal
         ctrl.openModal = function(modal_id, modal_size, modal_backdrop)
         {
-            $(".ms-selection").find("span:contains('" + ctrl.newSelectedRate + "')").css("color", "black");
             $rootScope.holidayRateModal = $modal.open({
                 templateUrl: modal_id,
                 size: modal_size,
                 backdrop: typeof modal_backdrop == 'undefined' ? true : modal_backdrop
             });
-            $rootScope.holidayRateModal.holidayManually = true;
+            $timeout(function() {
+                $('#holidayDate').datepicker()
+                        .on('changeDate', function(ev) {
+                            $rootScope.holidayRateModal.holiday.holidayDate = $filter('date')(ev.date, $rootScope.dateFormat);
+                        });
+            }, 150);
 
+            $rootScope.holidayRateModal.holiday = ctrl.newSelectedRate[0];
+            $rootScope.holidayRateModal.holiday.isRepeatAnnually = true;
+            $rootScope.holidayRateModal.holiday.holidayDate = $filter('date')(new Date(), $rootScope.dateFormat);
             $rootScope.holidayRateModal.save = function() {
                 $timeout(function() {
                     if ($('#holiday_rate_form')[0].checkValidity()) {
-                        if (!$rootScope.holidayRateModal.holidayManually) {
-                            $(".ms-selection").find("span:contains('" + ctrl.newSelectedRate + "')").css("color", "red");
-                        }
                         $rootScope.holidayRateModal.dismiss();
+                        if (ctrl.payrollObj.holidays == null) {
+                            ctrl.payrollObj.holidays = [];
+                        }
+                        angular.forEach(ctrl.payrollObj.holidays, function(holiday) {
+                            if (holiday.name == $rootScope.holidayRateModal.holiday.name) {
+                                holiday = $rootScope.holidayRateModal.holiday;
+                            }
+                        });
+                        ctrl.setHolidayManually();
                     }
                 });
             };
@@ -84,7 +107,8 @@
                 ctrl.payrollObj.holidays.splice(indexOfHoliday(ctrl.newSelectedRate[0]), 1);
                 $timeout(function() {
                     $("#multi-select").multiSelect('refresh');
-                })
+                    ctrl.setHolidayManually();
+                });
                 $rootScope.holidayRateModal.close();
             };
 
@@ -97,21 +121,33 @@
                 }
             }
         };
+        ctrl.resetPayroll = function() {
+            delete ctrl.payrollObj.overtimeThreshhold;
+            delete ctrl.payrollObj.vaccationRate;
+            delete ctrl.payrollObj.sickRate;
+            delete ctrl.payrollObj.personalRate;
+            delete ctrl.payrollObj.holidays;
+            $timeout(function() {
+                $("#multi-select").multiSelect('refresh');
+//                ctrl.setHolidayManually();
+            });
+        };
 
         $scope.$watch(function() {
             return ctrl.payrollObj.holidays;
         }, function(newValue, oldValue) {
             if (ctrl.displayHolidayModal && newValue != null && (oldValue == null || newValue.length > oldValue.length)) {
-                ctrl.openModal('modal-5', 'md', false);
                 if (oldValue == null) {
                     ctrl.newSelectedRate = newValue;
                 } else {
                     ctrl.newSelectedRate = arr_diff(newValue, oldValue);
                 }
+                ctrl.newSelectedRate[0].isRepeatAnnually = true;
+                ctrl.openModal('modal-5', 'md', false);
             }
         });
         ctrl.initSettings();
     }
     ;
-    angular.module('xenon.controllers').controller('PayrollSettingsCtrl', ["$rootScope", "$scope", "$http", "$modal", "$timeout", "PayrollDAO", PayrollSettingsCtrl]);
+    angular.module('xenon.controllers').controller('PayrollSettingsCtrl', ["$rootScope", "$scope", "$http", "$modal", "$timeout", "PayrollDAO", "$filter", PayrollSettingsCtrl]);
 })();
