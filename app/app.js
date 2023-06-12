b4c896093e65ac4c99e38ffc1cab8591ccccc184
usmanaryk1 <usmanaryk1@gmail.com>
2023-06-13 00:00:00 +0500

    release-3.7 preparation

--- app/app.js
+++ app/app.js
@@ -1576,7 +1576,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$3.6$@';
+                        config.params['version'] = '@$4.0$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
