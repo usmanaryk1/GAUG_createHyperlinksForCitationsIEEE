a24a5a62ebda9509c0fe257f26d19c23ba139511
usmanaryk1 <usmanaryk1@gmail.com>
2023-06-29 00:00:00 +0500

    version upgraded to 4.1.0

--- app/app.js
+++ app/app.js
@@ -1583,7 +1583,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$4.0$@';
+                        config.params['version'] = '@$4.1.0$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
