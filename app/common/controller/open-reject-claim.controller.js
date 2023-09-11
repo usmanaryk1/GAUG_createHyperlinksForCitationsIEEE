5122398bef8b93de26d5d04bf32f5cc0be3f52c2
usmanaryk1 <usmanaryk1@gmail.com>
2023-09-12 00:00:00 +0500

    added logic for reject claim other. handled error messages gracefully

--- app/common/controller/open-reject-claim.controller.js
+++ app/common/controller/open-reject-claim.controller.js
@@ -3,10 +3,18 @@
         var ctrl = this;
         ctrl.mode = mode;
         ctrl.claim = claim;
-        ctrl.codes = ["CO-196","CO-50","CO-45","OA-96","CO- 27","197","192","160","109","45","29","27","18","15","N522","N372","N362","N52"];
+        ctrl.codes = ["CO-196","CO-50","CO-45","OA-96","CO- 27","197","192","160","109","45","29","27","18","15","N522","N372","N362","N52","Other"];
         ctrl.details = {claimId: claim.id, status: (claim.isRejected === true ? 'open' : 'reject')};
         ctrl.ok = function () {
-            if ($('#openRejectModal')[0].checkValidity()) {
+            ctrl.submitted = true;
+            if (ctrl.openRejectModal.$valid) {
+                if (ctrl.details.rejectionCodeValue === 'Other') {
+                    ctrl.details.rejectionCode = ctrl.details.rejectionCodeOtherValue;
+                } else {
+                    ctrl.details.rejectionCode = ctrl.details.rejectionCodeValue;
+                }
+                delete ctrl.details.rejectionCodeOtherValue;
+                delete ctrl.details.rejectionCodeValue;
                 $modalInstance.close(ctrl.details);
             }
         };
