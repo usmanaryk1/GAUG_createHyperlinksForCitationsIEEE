4e354d4d6f13acc1468391b94197e0983891be7c
usmanaryk1 <usmanaryk1@gmail.com>
2023-10-19 00:00:00 +0500

    release 5.0.0 added

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function (grunt) {
     var config = {
         app: require('./package.json').appPath || 'app',
         name: require('./package.json').name || 'app',
-        currentVersion: /(@\$4.6.2\$@)/g,
-        targetVersion: '@$4.6.3$@'
+        currentVersion: /(@\$4.6.3\$@)/g,
+        targetVersion: '@$5.0.0$@'
     };
     // Define the configuration for all the tasks
     grunt.initConfig({
