07996e653265331cbce60256768691ba2c25f3e4
usmanaryk1 <usmanaryk1@gmail.com>
2023-11-14 00:00:00 +0500

    New Hires related changes on employee calendar and Search employee page

--- app/calendar/controller/calendar.controller.js
+++ app/calendar/controller/calendar.controller.js
@@ -156,6 +156,9 @@
             if (searchParams.drives != null && !searchParams.drives) {
                 delete searchParams.drives;
             }
+            if (searchParams.newHires != null && !searchParams.newHires) {
+                delete searchParams.newHires;
+            }
             if (searchParams.scheduledOnly != null && searchParams.scheduledOnly) {
                 searchParams.scheduleStartDate = $filter('date')($rootScope.weekStart, $rootScope.dateFormat);
                 searchParams.scheduleEndDate = $filter('date')($rootScope.weekEnd, $rootScope.dateFormat);
