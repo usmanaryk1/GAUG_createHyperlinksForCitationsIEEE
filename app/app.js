ffc8e56851edf82bf12d052099e90b3a85a06e46
usmanaryk1 <usmanaryk1@gmail.com>
2023-11-21 00:00:00 +0500

    release version changed to 5.1.1

--- app/app.js
+++ app/app.js
@@ -1702,7 +1702,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$5.1.0$@';
+                        config.params['version'] = '@$5.1.1$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
