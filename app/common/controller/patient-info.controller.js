3d9ff6b522e8eb8ba5c722b4012c2fbaae17c15b
usmanaryk1 <usmanaryk1@gmail.com>
2023-12-14 00:00:00 +0500

    aide skills added on patient pop up

--- app/common/controller/patient-info.controller.js
+++ app/common/controller/patient-info.controller.js
@@ -1,12 +1,14 @@
 (function () {
-    function PatientInfoCtrl(patientId,insuranceProviderMap,nursingCareMap,staffCoordinatorMap,$rootScope,$modal,$modalInstance,PatientDAO) {
+    function PatientInfoCtrl(patientId, insuranceProviderMap, nursingCareMap, staffCoordinatorMap, $rootScope, $modal, $modalInstance, PatientDAO) {
         var ctrl = this;
-        
+
         ctrl.companyCode = ontime_data.company_code;
         ctrl.baseUrl = ontime_data.weburl;
         ctrl.patient = {};
         $rootScope.maskLoading();
-        PatientDAO.getPatientsForSchedule({patientIds: patientId, addressRequired: true}).then(function (patients) {
+        PatientDAO.retrieveEnumType({'type': 'aideSkills'}).then(function (res) {
+            ctrl.capacitiesKeyValue = res;
+            PatientDAO.getPatientsForSchedule({patientIds: patientId, addressRequired: true}).then(function (patients) {
                 var patient = patients[0];
                 ctrl.patient = angular.copy(patient);
                 ctrl.patient.insuranceProviderName = insuranceProviderMap[patient.insuranceProviderId];
@@ -15,13 +17,25 @@
                 if (patient.languagesSpoken != null && patient.languagesSpoken.length > 0) {
                     ctrl.patient.languagesSpoken = patient.languagesSpoken.split(",");
                 }
+                var aideSkills = ctrl.patient.aideSkills.split(',');
+                var patientAideSkills = [];
+                angular.forEach(aideSkills, function (skillId) {
+                    if (skillId) {
+                        patientAideSkills.push(skillId);
+                    }
+                });
+                ctrl.patient.patientAideSkills = patientAideSkills;
                 $rootScope.unmaskLoading();
             });
-                                
+        }).catch(function () {
+            toastr.error("Failed to retrieve aide skill capacities.");
+        });
+
+
         ctrl.close = function () {
             $modalInstance.close();
         };
     }
     ;
-    angular.module('xenon.controllers').controller('PatientInfoCtrl', ["patientId","insuranceProviderMap","nursingCareMap","staffCoordinatorMap","$rootScope", "$modal","$modalInstance","PatientDAO", PatientInfoCtrl]);
+    angular.module('xenon.controllers').controller('PatientInfoCtrl', ["patientId", "insuranceProviderMap", "nursingCareMap", "staffCoordinatorMap", "$rootScope", "$modal", "$modalInstance", "PatientDAO", PatientInfoCtrl]);
 })();
