ed6ab9da49513f5c68610110128bf7aff4ab9b1f
usmanaryk1 <usmanaryk1@gmail.com>
2023-12-15 00:00:00 +0500

    release 5.1.9 created

--- app/app.js
+++ app/app.js
@@ -1702,7 +1702,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$5.1.8$@';
+                        config.params['version'] = '@$5.1.9$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
