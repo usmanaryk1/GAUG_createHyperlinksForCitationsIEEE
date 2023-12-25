0996aa709e6e42e4e69de5fbf6a13d90779ac9bd
usmanaryk1 <usmanaryk1@gmail.com>
2023-12-26 00:00:00 +0500

    6.0.1 release changes

--- app/app.js
+++ app/app.js
@@ -1702,7 +1702,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$6.0.0$@';
+                        config.params['version'] = '@$6.0.1$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
