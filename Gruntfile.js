77ab7e272d1b70e12f5327d000fcd79e6164076b
usmanaryk1 <usmanaryk1@gmail.com>
2024-01-02 00:00:00 +0500

    branch created for 6.0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function (grunt) {
     var config = {
         app: require('./package.json').appPath || 'app',
         name: require('./package.json').name || 'app',
-        currentVersion: /(@\$6.0.1\$@)/g,
-        targetVersion: '@$6.0.2$@'
+        currentVersion: /(@\$6.0.2\$@)/g,
+        targetVersion: '@$6.0.3$@'
     };
     // Define the configuration for all the tasks
     grunt.initConfig({
