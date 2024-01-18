cc6a62189100e85c25095ea6cf267f904818852d
usmanaryk1 <usmanaryk1@gmail.com>
2024-01-19 00:00:00 +0500

    version upgraded to 6.0.8

--- app/app.js
+++ app/app.js
@@ -1851,7 +1851,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$6.0.7$@';
+                        config.params['version'] = '@$6.0.8$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
