de0dfdd4a95fadcc2e1905082ff89514891ee0a8
usmanaryk1 <usmanaryk1@gmail.com>
2024-01-25 00:00:00 +0500

    6.0.10 created

--- app/app.js
+++ app/app.js
@@ -1851,7 +1851,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, AS
                         if (!config.params) {
                             config.params = {};
                         }
-                        config.params['version'] = '@$6.0.9$@';
+                        config.params['version'] = '@$6.0.10$@';
                     }
                     ontime_data.company_code = getCookie("cc");
                     if (ontime_data.company_code != null) {
