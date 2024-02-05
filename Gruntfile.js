06921703c6e6b03e1dab4eaf16eb9e8410609d39
usmanaryk1 <usmanaryk1@gmail.com>
2024-02-06 00:00:00 +0500

    6.13.0 version added

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function (grunt) {
     var config = {
         app: require('./package.json').appPath || 'app',
         name: require('./package.json').name || 'app',
-        currentVersion: /(@\$6.0.11\$@)/g,
-        targetVersion: '@$6.0.12$@'
+        currentVersion: /(@\$6.0.12\$@)/g,
+        targetVersion: '@$6.0.13$@'
     };
     // Define the configuration for all the tasks
     grunt.initConfig({
