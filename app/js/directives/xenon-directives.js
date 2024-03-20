4ae7255bea3184912aa834e08d2aa94025667021
usmanaryk1 <usmanaryk1@gmail.com>
2024-03-21 00:00:00 +0500

    OWA-177 - Directive to restrict hyphen in company info.

--- app/js/directives/xenon-directives.js
+++ app/js/directives/xenon-directives.js
@@ -1337,4 +1337,34 @@ angular.module('xenon.directives', []).
                 }
             }
 
+        })
+        .directive('restrictHyphen', function () {
+            return {
+                require: '?ngModel',
+                link: function (scope, element, attrs, ngModelCtrl) {
+                    if (!ngModelCtrl) {
+                        return;
+                    }
+
+                    ngModelCtrl.$parsers.push(function (val) {
+                        if (angular.isUndefined(val)) {
+                            var val = '';
+                        }
+                        //Get rid of hypen
+                        var clean = val.replace(/[-]/g, '');
+                        
+                        if (val !== clean) {
+                            ngModelCtrl.$setViewValue(clean);
+                            ngModelCtrl.$render();
+                        }
+                        return clean;
+                    });
+                    //Prevent space key press
+                    element.bind('keypress', function (event) {
+                        if (event.keyCode === 32) {
+                            event.preventDefault();
+                        }
+                    });
+                }
+            };
         });
