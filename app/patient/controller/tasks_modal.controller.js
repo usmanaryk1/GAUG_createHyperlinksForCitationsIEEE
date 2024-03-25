798903bb723653f48f32c8ce76785c69a99591d1
usmanaryk1 <usmanaryk1@gmail.com>
2024-03-25 12:00:00 +0500

    add day functionality

--- app/patient/controller/tasks_modal.controller.js
+++ app/patient/controller/tasks_modal.controller.js
@@ -5,7 +5,7 @@
         
         // console.log("selectedType, taskScheduleSet, editMode",selectedType, taskScheduleSet, editMode,);
         // Define the days of the week
-        ctrl.daysOfWeek = ['Su', 'Mo',];
+        ctrl.daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
         
         ctrl.generateFormCall = generateForms;
