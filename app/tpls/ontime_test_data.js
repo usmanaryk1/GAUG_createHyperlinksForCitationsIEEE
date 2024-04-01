e1e1a3e3c3dcb08db47b26708f40ef956e7c2204
usmanaryk1 <usmanaryk1@gmail.com>
2024-04-02 00:00:00 +0500

    fixes- employee worked hours report added

--- app/tpls/ontime_test_data.js
+++ app/tpls/ontime_test_data.js
@@ -307,6 +307,7 @@ ontimedata = {
         {id: 'patientcensus', label: "Patient Census"},
         {id: 'patienttimesheet', label: "Patient Time Sheet"},
         {id: 'workedhours', label: "Worked Hours"},
+        {id: 'employeeworkedhoursbycounty', label: "Employee Worked Hours - By County"}
     ],
     unitValues: [{value: 0.25, label: "15 min"}, {value: 0.5, label: "30 min"}, {value: 0.45, label: "45 min"}, {value: 1, label: "1 hr"}],
 };
