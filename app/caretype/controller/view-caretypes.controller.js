35a90efc46c4e1ddec3718a66d0b3673f166a37e
usmanaryk1 <usmanaryk1@gmail.com>
2024-05-02 00:00:00 +0500

    added caretype changes

---
+++ app/caretype/controller/view-caretypes.controller.js
@@ -0,0 +1,87 @@
+(function () {
+    function ViewCareTypesCtrl(CareTypeDAO, $rootScope, $stateParams, $state, $modal, Page, $debounce, $timeout, $formService, PositionDAO) {
+        var ctrl = this;
+        
+        function initialize(){
+            //$rootScope.maskLoading();
+            Page.setTitle("Care Types");
+            ctrl.companyCode = ontime_data.company_code;
+            ctrl.baseUrl = ontime_data.weburl;
+
+            ctrl.careTypeList = [];
+            ctrl.positions = [];
+            ctrl.test ='';
+            
+            ctrl.retrieveCareTypes = retrieveCareTypes;
+            ctrl.addEditPopup = addEditPopup;
+            ctrl.getPositions = getPositions;
+            //ctrl.save = save;
+            //ctrl.activateDeactivatePopup = activateDeactivatePopup;
+            //ctrl.activateDeactivatePosition = activateDeactivatePosition;
+            //ctrl.changeStatus = changeStatus;
+            ctrl.retrieveCareTypes();
+        }
+
+        function getPositions() {
+            PositionDAO.view({subAction: 'active'}).then(function (res) {
+                $rootScope.careTypeModel.positions = res;
+            });
+        };
+
+        function retrieveCareTypes(){
+            CareTypeDAO.view({subAction: 'all'}).then(function (res) {
+                showLoadingBar({
+                    delay: .5,
+                    pct: 100,
+                    finish: function () {
+                    }
+                }); // showLoadingBar
+                ctrl.careTypeList = res;
+            }).catch(function (data, status) {
+                toastr.error("Failed to retrieve caretypes.");
+                showLoadingBar({
+                    delay: .5,
+                    pct: 100,
+                    finish: function () {
+
+                    }
+                }); // showLoadingBar
+                console.log('Error in retrieving data')
+            }).then(function () {
+                $rootScope.unmaskLoading();
+            });
+        }
+
+        function addEditPopup(caretype){
+            var caretypeCopy = angular.copy(caretype);
+            $rootScope.careTypeModel = $modal.open({
+                templateUrl: 'careTypeModel'
+            });
+
+            if(caretypeCopy == undefined) { 
+                $rootScope.careTypeModel.caretype = {};
+                $rootScope.careTypeModel.caretype.action = 'savecaretype';
+            }else{
+                $rootScope.careTypeModel.caretype = caretypeCopy;
+                $rootScope.careTypeModel.caretype.action = 'updatecaretype';
+            }
+
+            ctrl.getPositions();
+            
+            
+            $rootScope.careTypeModel.closePopup = function(){
+                $rootScope.careTypeModel.close();
+            }
+
+            $rootScope.careTypeModel.save = function(){
+                if($rootScope.careTypeModel.caretype_form.$valid){
+                    ctrl.save($rootScope.careTypeModel.caretype);   
+                }                
+            }
+        }
+
+        initialize();
+    };
+
+    angular.module('xenon.controllers').controller('ViewCareTypesCtrl', ["CareTypeDAO", "$rootScope", "$stateParams", "$state", "$modal", "Page", "$debounce", "$timeout", "$formService", "PositionDAO", ViewCareTypesCtrl]);
+})();
