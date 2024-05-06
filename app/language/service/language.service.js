d9d502628b52443039cf31d8d453e6d99693abcc
usmanaryk1 <usmanaryk1@gmail.com>
2024-05-07 00:00:00 +0500

    Added code for languages

---
+++ app/language/service/language.service.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+    var LanguageDAO = function(resource) {
+        var api = resource(ontime_data.weburl + 'admin/language/:action/:subAction/:subAction1', {}, {
+            view: {
+                method: 'GET',
+                params: {
+                    action: 'viewAll'
+                },
+                isArray: true
+            },
+
+            changestatus: {
+                method: 'GET'
+            }
+        });
+        return {
+            view: function (filter) {
+                return api.view(filter).$promise;
+            },
+            changestatus: function (data) {
+                return api.changestatus({action: 'changestatus', subAction: data.id, status: data.status}).$promise;
+            },
+        };
+    };
+    angular.module("xenon.factory").factory('LanguageDAO', ['$resource', LanguageDAO]);
+})();
